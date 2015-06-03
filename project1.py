449ec612dab0106af9a2abb1dde58af909e37e5e
keithgw <keith.gordon.williams@gmail.com>
2015-06-03 13:51:27 -0400

    added Part 2: in_degree functions

--- project1.py
+++ project1.py
@@ -1,6 +1,8 @@
 """Project 1 completed as part of Algorithmic Thinking on Coursera from Rice
 University. May 27, 2015 run of the course"""
 
+## Part 1: Representing directed graphs
+
 # Define directed graphs as dictionaries
 EX_GRAPH0 = {0: set([1, 2]), 1: set([]), 2: set([])}
 
@@ -22,10 +24,57 @@ def make_complete_graph(num_nodes):
     
     # define list of nodes
     nodes = range(0, num_nodes)
-        
+     
+    # loop over nodes and set values for each key       
     for node in nodes:
         outs = set(nodes)
         outs.remove(node)
         graph[node] = outs
         
     return graph
+
+## Part 2: Computing degree distributions
+
+def compute_in_degrees(digraph):
+    """Takes a directed graph represented as a dictionary and computes the 
+    in-degrees for the nodes in the graph. Returns a dictionary with the same 
+    set of keys (nodes) as digraph whose corresponding values are the number of
+    edges whose head matches a particular node."""
+    
+    # define empty dictionary
+    in_degrees = {}
+    
+    # define keys as nodes in digraph
+    nodes = [key for key in digraph]
+    
+    # loop over nodes and count number of times each node appears in the
+    # values of other nodes in digraph
+    for node in nodes:
+        count = 0
+        for ndoe in nodes:
+            if node in digraph[ndoe]:
+                count += 1
+        in_degrees[node] = count
+    
+    return in_degrees
+    
+def in_degree_distribution(digraph):
+    """Takes a directed graph represented as a dictionary and comptes the
+    unnormalized distribution of the in-degrees of the graph. Returns a 
+    dictionary whose keys correspond to in-degrees of nodes in the graph. The
+    value associated with each particular in-degree is the number of nodes with
+    that in-degree. In-degrees with no corresponding nodes in the graph are not
+    included in the dictionary."""
+    
+    # compute the in degree for each node in the directed graph
+    in_degrees = compute_in_degrees(digraph)
+    
+    # loop over nodes, add value to distribution dictionary
+    distro = {}
+    for node in in_degrees:
+        if in_degrees[node] in distro:
+            distro[in_degrees[node]] += 1
+        else:
+            distro[in_degrees[node]] = 1
+    
+    return distro
