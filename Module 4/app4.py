acfa45b2e6900451644dd29b39c201493cf0e71b
keithgw <keith.gordon.williams@gmail.com>
2015-08-04 06:44:18 -0400

    App 4 completed, fast spell check implemented

--- Module 4/app4.py
+++ Module 4/app4.py
@@ -176,7 +176,7 @@ def plot_htest(scores):
     amino acid sequence alignments.
     """
     plt.figure()
-    plt.hist(scores, normed=True, color='c')
+    plt.hist(scores, normed=True, color='c', bins = 35)
     plt.title('Histogram of Scores of Aligned, Randomly Permuted AA Sequences')
     plt.xlabel('Score of Local Alignment')
     plt.ylabel('Normalized Frequency')
@@ -196,7 +196,89 @@ def zscore(scores, h_a):
     zscore = (h_a - mu) / sigma
     return (mu, sigma, zscore)
     
-            
+def find_scoring_matrix(x, y, med, dim):
+    """
+    Find the scoring matrix that satisifes the definition of minimum edit
+    distance: |x| + |y| - score(x, y)
+    
+    Inputs:
+        x, y: english strings
+        med: minimum edit distance between x, y
+        dim: range of values to test for diag_score, off_score, dash_score
+            note dash_scores will be <= 0
+    """
+    alphabet = set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+    
+    # med(kitten, sitting) = 3
+    correct = len(x) + len(y) - med # 10
+    solutions = np.zeros((dim, dim, dim))
+    for diag in range(dim):
+        for off in range(dim):
+            for dash in range(dim):
+                sm = seq.build_scoring_matrix(alphabet, diag, off, -1 * dash)
+                am = seq.compute_alignment_matrix(x, y, sm)
+                solutions[diag, off, dash] = seq.compute_global_alignment(x, y, sm, am)[0]
+    
+    parameters = np.transpose(np.nonzero(solutions == correct))
+    parameters[:, 2] *= -1
+    return parameters
+    
+def check_spelling(checked_word, dist, word_list):
+    """
+    Returns a set of words from word_list that are dist edit distance from 
+    checked_word
+    """
+    alphabet = set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+    candidates = set([])
+        
+    for word in word_list:
+        smtrx = seq.build_scoring_matrix(alphabet, 2, 1, 0)
+        amtrx = seq.compute_alignment_matrix(checked_word, word, smtrx)
+        score = seq.compute_global_alignment(checked_word, word, smtrx, amtrx)[0]
+        if len(checked_word) + len(word) - score <= dist:
+            candidates.add(word)
+                
+    return candidates
+        
+
+def edits1(word):
+    """
+    Creates all 1-edit variants of checked_word.
+    """
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    n = len(word)
+    splits = [(word[:i], word[i:]) for i in range(n + 1)]
+    deletes = [a + b[1:] for a, b in splits if b]
+    inserts = [a + c + b for a, b in splits for c in alphabet]
+    substitutions = [a + c + b[1:] for a, b in splits for c in alphabet if b]
+    return set(deletes + inserts + substitutions)
+                        
+def fast_spell_check(checked_word, word_list):
+    """
+    Returns a set of words from word_list with edit distance <= 2 from
+    checked_word.
+    """
+    # store word_list as a set for fast access
+    corpus = set(word_list)
+    
+    # create all 1-edit variants of checked_word
+    edits_1 = edits1(checked_word)
+    
+    # create all 1-edit variants of 1-edit variants of checked_word
+    edits_2 = set([])
+    for edit in edits_1:
+        edits_1_1 = edits1(edit)
+        edits_2 = edits_2.union(edits_1_1)
+        
+    edits = set([checked_word]).union(edits_1).union(edits_2)
+    
+    # return only the words from word_list that are in 0-, 1-, and 2-edit variants
+    return edits.intersection(corpus)
+    
+
+
 def run():
     
     # load sequences and scoring matrix
@@ -224,10 +306,41 @@ def run():
     
     # Question 5
     htest = zscore(h_0, local_alignment[0])
+    print 'Hypothesis test:'
     print 'mean: {:.3f} standard deviation: {:.3f} z-score: {:.3f}'.format(htest[0], htest[1], htest[2])
-
+    print '=' * 30
+    
+    # Question 6
+    plot_htest(h_0)    
+    
+    # Question 7
+    kitten_sitting = find_scoring_matrix('kitten', 'sitting', 3, 3)
+    intention_execution = find_scoring_matrix('intention', 'execution', 5, 3)
+    for solution in kitten_sitting:
+        for _solution in intention_execution:
+            if np.all(solution == _solution):
+                print 'Solution to Scoring Matrix Paramters for Edit Distance:'
+                print '[diag_score  off_diag_score  dash_score]'
+                print solution
+                print '=' * 30
+                
+    # Question 8
+    word_list = read_words(WORD_LIST_URL)
+    print 'Within 1 of "humble": \n'
+    print check_spelling('humble', 1, word_list)
+    print '=' * 30
+    print 'Within 2 of "firefly": \n'
+    print check_spelling('firefly', 2, word_list)
+    print '=' * 30
+    
+    # Question 9
+    print 'Within 1 of "humble" (fast): \n'
+    print set(word_list).intersection(edits1('humble'))
+    print '=' * 30
+    print 'Within 2 of "firefly" (fast): \n'
+    print fast_spell_check('firefly', word_list)
     
     
+if __name__ == '__main__':
+    run()
     
-    
-        
